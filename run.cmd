3994991964e00549515bd6b78bfbe7604529a08a
BZ <BZ>
2015-12-09 15:18:20 -0800

    Updated Run to Launch Browser

--- run.cmd
+++ run.cmd
@@ -5,8 +5,10 @@ echo "Creating MongoDB Directory"
 md C:\Mongo-DB\data
 echo "Starting MongoDB"
 start "" "C:\Program Files\MongoDB\Server\3.2\bin\mongod.exe" --dbpath C:\Mongo-DB\data 
-# call npm install -g nodemon
+REM call npm install -g nodemon
 echo "Installing NPM Modules"
 call npm install
 echo "Starting Portal"
-nodemon
+REM nodemon
+start "" node server.js
+start "" "http://localhost"
